9dd9d9d3c6938c763df3bc8ee85bfd40c09836b8
rnlisboa <dev.renanlisboa@gmail.com>
2024-12-31 11:02:14 -0300

    feat: start project

---
+++ src/main/java/com/example/backend_payment_app/BackendPaymentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_payment_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPaymentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPaymentAppApplication.class, args);
+	}
+
+}
